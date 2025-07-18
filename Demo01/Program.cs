ca8415d7e7b5f397d26ff437bb8740bc0359ec69
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-07-19 00:42:52 +0300

    Video02

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -5,27 +5,46 @@
         static void Main(string[] args)
         {
             #region Struct
-            //Point
-            Point P01; //Declare for object from type 'Point'
-            P01.X = P01.Y = 12;
+            ////Point
+            //Point P01; //Declare for object from type 'Point'
+            //P01.X = P01.Y = 12;
 
-            Console.WriteLine(P01.X); //12
-            Console.WriteLine(P01.Y); //12
+            //Console.WriteLine(P01.X); //12
+            //Console.WriteLine(P01.Y); //12
 
-            //initialize with calling built-in or user-defined constructor
-            Point P02 = new Point(); //Point P02 = new Point();
-            Console.WriteLine(P02.X);
-            Console.WriteLine(P02.Y);
+            ////initialize with calling built-in or user-defined constructor
+            //Point P02 = new Point(); //Point P02 = new Point();
+            //Console.WriteLine(P02.X);
+            //Console.WriteLine(P02.Y);
 
-            //initialize with calling built-in or user-defined constructor
-            Point P03 = new Point(1, 2); //Point P02 = new Point();
-            Console.WriteLine(P03.X); //1
-            Console.WriteLine(P03.Y); //2
+            ////initialize with calling built-in or user-defined constructor
+            //Point P03 = new Point(1, 2); //Point P02 = new Point();
+            //Console.WriteLine(P03.X); //1
+            //Console.WriteLine(P03.Y); //2
 
-            Console.WriteLine(P03); //Assignment01.Point
+            //Console.WriteLine(P03); //Demo01.Point
 
-            P03.PrintPoint(); //(1,2)
-            Console.WriteLine(P03.ToString()); //(1, 2) 
+            //P03.PrintPoint(); //(1,2)
+            //Console.WriteLine(P03.ToString()); //(1, 2) 
+
+            #endregion
+
+            #region Encapsulation Using Setter and Getter
+            //Employee E01 = new Employee(1, "Hussein", -100);
+            //E01.SetId(1);
+            //E01.SetName("Hussein");
+            //E01.SetSalary(-100);
+
+            //Console.WriteLine(E01.GetId());
+            //Console.WriteLine(E01.GetName());
+            //Console.WriteLine(E01.GetSalary());
+
+            Employee E02 = new(2, "Mahmoud", 20000);
+            Console.WriteLine(E02.GetId());
+            Console.WriteLine(E02.GetName());
+            Console.WriteLine(E02.GetSalary());
+
+            Console.WriteLine(E02);
 
             #endregion
         }
